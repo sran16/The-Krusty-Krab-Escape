d6681e72bfb2367e491ccfad8a0fdfbe0ab8e80b
sran16 <ranimsaidi888@gmail.com>
2023-11-07 23:59:30 +0100

    test animation plankton + exo php

--- src/Preloader.js
+++ src/Preloader.js
@@ -1,5 +1,10 @@
 class Preloader {
   constructor(scene) {
     scene.load.image("obstacle-scene-un", "assets/obstacle-scene-un.png");
+    scene.load.spritesheet("plankton", "assets/plankton.png", {
+      frameWidth: 32,
+      frameHeight: 32,
+    });
+
   }
 }
--- src/Scene/Scene.js
+++ src/Scene/Scene.js
@@ -1,11 +1,33 @@
 class Scene extends Phaser.Scene {
+  constructor() {
+    super();
+    this.planktonSprite = null;
+  }
+
   preload() {
-    new Preloader(this);
-    this.load.image("scene-start", "assets/start.png");
+    this.load.image("obstacle-scene-un", "assets/obstacle-scene-un.png");
+    // Load the image for the plankton sprite.
+    this.load.image("plankton", "assets/plankton.png");
   }
 
   async create() {
-    this.add.image(640, 360, "scene-start");
-    this.add.image(400, 20, "obstacle-scene-un");
+    // Create the plankton sprite.
+    this.planktonSprite = this.add.sprite(100, 100, "plankton");
+
+    // Configure the properties of the plankton sprite.
+    this.planktonSprite.setSize(32, 32);
+    this.planktonSprite.setBounce(0.2);
+    this.planktonSprite.setGravityY(300);
+
+    // Create an animation named "run" for the plankton sprite.
+    this.anims.create({
+      key: "run",
+      frames: this.anims.generateFrameNumbers("plankton", { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1, // -1 means it will loop forever
+    });
+
+    // Play the "run" animation for the plankton sprite.
+    this.planktonSprite.play("run");
   }
 }
