c29180a4a0ba454ee1f9b3c4cecfb17409390240
sran16 <ranimsaidi888@gmail.com>
2023-11-10 15:31:13 +0100

    Fix: Wrong choices rebase

--- src/Scene/SceneFinale.js
+++ src/Scene/SceneFinale.js
@@ -1,44 +1,41 @@
 // Define a Phaser Scene named "SceneFinale"
 
-class SceneFinale extends Phaser.Scene {
-  
+export default class SceneFinale extends Phaser.Scene {
+  constructor() {
+    super({ key: "SceneFinale" });
+  }
+
   preload() {
     new Preloader(this);
   }
-   
-    async create() {
 
-      // images for the finale scene
-      const SceneFinale = this.add.image(760, 415, "background-scene-finale"); 
-      SceneFinale.setScale(1.20);
+  async create() {
+    // images for the finale scene
+    const SceneFinale = this.add.image(760, 415, "background-scene-finale");
+    SceneFinale.setScale(1.2);
 
+    const endText = this.add.image(1150, 600, "end-text");
+    endText.setScale(1.1);
+    endText.setAlpha(0);
 
-      const endText = this.add.image(1150, 600, "end-text"); 
-      endText.setScale(1.1);
-      endText.setAlpha(0);
+    const planktonEnd = this.add.image(870, 650, "plankton-final");
+    planktonEnd.setScale(0.2);
+    planktonEnd.setAlpha(0);
 
+    // animation for the end message and plankton
 
-      const planktonEnd = this.add.image(870, 650, "plankton-final");
-      planktonEnd.setScale(0.2); 
-      planktonEnd.setAlpha(0);
-
-      // animation for the end message and plankton 
-
-
-      this.tweens.add({
-        targets: endText,
-        alpha: 1, 
-        ease: 'Linear',   
-        duration: 1000,    
+    this.tweens.add({
+      targets: endText,
+      alpha: 1,
+      ease: "Linear",
+      duration: 1000,
     });
 
     this.tweens.add({
       targets: planktonEnd,
-      alpha: 1, 
-      ease: 'Linear',   
-      duration: 1000,    
-  });
-
-    }
-
+      alpha: 1,
+      ease: "Linear",
+      duration: 1000,
+    });
+  }
 }
